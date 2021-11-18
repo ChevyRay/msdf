e2265c25689fee691f35e0920e4a4dcea6f7b0b2
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-17 21:26:36 -0800

    signed_distance struct

---
+++ src/signed_distance.rs
@@ -0,0 +1,34 @@
+use std::cmp::Ordering;
+
+#[derive(PartialEq)]
+pub struct SignedDistance {
+    distance: f64,
+    dot: f64,
+}
+
+impl SignedDistance {
+    #[inline]
+    pub const fn new(distance: f64, dot: f64) -> Self {
+        Self { distance, dot }
+    }
+
+    #[inline]
+    pub const fn infinite() -> Self {
+        Self::new(-1e240, 1.0)
+    }
+}
+
+impl PartialOrd for SignedDistance {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let a_dist = self.distance.abs();
+        let b_dist = self.distance.abs();
+        if a_dist < b_dist {
+            Some(Ordering::Less)
+        } else if a_dist > b_dist {
+            Some(Ordering::Greater)
+        } else {
+            self.dot.partial_cmp(&other.dot)
+        }
+    }
+}
